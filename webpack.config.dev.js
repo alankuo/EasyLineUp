6eea977258b8220b16f1fed54cc0ef10e1d5e5b9
Chaolei Hua <erichua03@gmail.com>
2018-03-18 10:20:41 -0700

    local css

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -26,7 +26,8 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /\.module.css$/, loader: 'style-loader!css-loader?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]'},
+      {test: /^((?!\.module).)*css$/, loader: 'style!css'},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
